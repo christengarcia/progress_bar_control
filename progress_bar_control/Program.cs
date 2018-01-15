59d0b38f9df410bd9451514a0505230238811876
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-15 20:37:45 +1100

    Initial commit

---
+++ progress_bar_control/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * C# Program to Create a Progress Bar Control
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace progress_bar_control
+{
+    
